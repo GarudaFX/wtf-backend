41007d628ff11fe234e869478bcea6ab03e2880f
GarudaFX <francisjohnt24@gmail.com>
2023-11-18 16:07:01 +0800

    remove middlewares for the sake of presentation

--- routes/api.php
+++ routes/api.php
@@ -33,49 +33,44 @@ Route::group(['prefix' => 'auth'], function () {
     });
 });
 
-Route::controller(PaymentController::class)
-    ->group(function () {
-        //logs of the students about their payment
-        Route::get('/student-logs/{student_id}', 'getStudentLogs');
-        //get all the list of payments of college fee of a specific college
-        Route::get('/get-all-payment/{college_id}', 'getAllPaymentOfCollege');
-        // search a specific student
-        Route::get('/search-student/{student_id}', 'searchStudent');
-        //add new payment of college fee
-        Route::post('/add-payment', 'addPayment');
-        //get the total collection of college fee of a specific college
-        Route::get('/get-total-payment/{college_id}', 'getTotalPayment');
-        //get all payment
-        Route::get('/get-all-payment', 'getAllPayment');
-        //this is for scanning the qr code of the student, to get the student id
-        Route::get('/get-student-payment/{id}', 'getStudentBalance');
-        //get the latest payment
-        Route::get('/latest-payment', 'getLastPaymentAr');
-        //get the last 7 days of collection of a specific college with percentange
-        Route::get('/last-7-days/{id}', 'getPercentageOfLast7daysCollection');
-        //get the last 30 days of collection of a specific college with percentange
-        Route::get('/last-30-days/{id}', 'getPercentageOfLast30daysCollection');
-        //get the total amount of already paid of a specific student
-        Route::get(
-            '/get-total-student-payment/{id}',
-            'getTotalPaymentOfStudent'
-        );
-        //get the total amount collected per month in a specific college
-        Route::get(
-            '/get-total-payment-per-month/{id}',
-            'getTotalPaymentPerMonthInCurrentYear'
-        );
-        //save the receipt of the student
-        Route::post('/save-receipt/{ar_no}', 'savePayment');
-    });
+Route::controller(PaymentController::class)->group(function () {
+    //logs of the students about their payment
+    Route::get('/student-logs/{student_id}', 'getStudentLogs');
+    //get all the list of payments of college fee of a specific college
+    Route::get('/get-all-payment/{college_id}', 'getAllPaymentOfCollege');
+    // search a specific student
+    Route::get('/search-student/{student_id}', 'searchStudent');
+    //add new payment of college fee
+    Route::post('/add-payment', 'addPayment');
+    //get the total collection of college fee of a specific college
+    Route::get('/get-total-payment/{college_id}', 'getTotalPayment');
+    //get all payment
+    Route::get('/get-all-payment', 'getAllPayment');
+    //this is for scanning the qr code of the student, to get the student id
+    Route::get('/get-student-payment/{id}', 'getStudentBalance');
+    //get the latest payment
+    Route::get('/latest-payment', 'getLastPaymentAr');
+    //get the last 7 days of collection of a specific college with percentange
+    Route::get('/last-7-days/{id}', 'getPercentageOfLast7daysCollection');
+    //get the last 30 days of collection of a specific college with percentange
+    Route::get('/last-30-days/{id}', 'getPercentageOfLast30daysCollection');
+    //get the total amount of already paid of a specific student
+    Route::get('/get-total-student-payment/{id}', 'getTotalPaymentOfStudent');
+    //get the total amount collected per month in a specific college
+    Route::get(
+        '/get-total-payment-per-month/{id}',
+        'getTotalPaymentPerMonthInCurrentYear'
+    );
+    //save the receipt of the student
+    Route::post('/save-receipt/{ar_no}', 'savePayment');
+});
 
 //admin apis ===================================================================
 
-Route::controller(AdminController::class)
-    ->group(function () {
-        //get all the admins under a specific college
-        Route::get('/get-admin/{college_id}', 'getAllAdminSpecificCollege');
-    });
+Route::controller(AdminController::class)->group(function () {
+    //get all the admins under a specific college
+    Route::get('/get-admin/{college_id}', 'getAllAdminSpecificCollege');
+});
 
 //program apis ===================================================================
 
@@ -86,53 +81,46 @@ Route::controller(ProgramController::class)->group(function () {
 
 //expenses apis ===================================================================
 
-Route::controller(ExpensesController::class)
-    ->group(function () {
-        //get all expenses of a specific college
-        Route::get('/expenses/{college_id}', 'getAllExpensesInSpecificCollege');
-        //add new expenses
-        Route::post('/add-expenses/{college_id}', 'addNewExpenses');
-    });
+Route::controller(ExpensesController::class)->group(function () {
+    //get all expenses of a specific college
+    Route::get('/expenses/{college_id}', 'getAllExpensesInSpecificCollege');
+    //add new expenses
+    Route::post('/add-expenses/{college_id}', 'addNewExpenses');
+});
 
 //receipt apis ===================================================================
 
-Route::controller(ReceiptController::class)
-    ->group(function () {
-        //get all receipts
-        Route::get('/receipts', 'getReceipts');
-        //get all information of receipt
-        Route::get('/receipt/{ar_no}', 'getFullDetailsOfReceipt');
-    });
+Route::controller(ReceiptController::class)->group(function () {
+    //get all receipts
+    Route::get('/receipts', 'getReceipts');
+    //get all information of receipt
+    Route::get('/receipt/{ar_no}', 'getFullDetailsOfReceipt');
+});
 
 //permission apis ===================================================================
-
-Route::controller(PermissionController::class)
-    /
-    ->group(function () {
-        //get all permissions
-        Route::get('/permissions', 'getAllPermissionsOfAllAdmins');
-        //update permission of an admin
-        Route::put('/can-update-permission/{admin_id}', 'canUpdatePermission');
-        Route::put('/can-delete-permission/{admin_id}', 'canDeletePermission');
-    });
+Route::controller(PermissionController::class)->group(function () {
+    //get all permissions
+    Route::get('/permissions', 'getAllPermissionsOfAllAdmins');
+    //update permission of an admin
+    Route::put('/can-update-permission/{admin_id}', 'canUpdatePermission');
+    Route::put('/can-delete-permission/{admin_id}', 'canDeletePermission');
+});
 
 //Logs apis ===================================================================
-Route::controller(LogsController::class)
-    ->group(function () {
-        //get all the logs
-        Route::get('/logs', 'getLogs');
-    });
+Route::controller(LogsController::class)->group(function () {
+    //get all the logs
+    Route::get('/logs', 'getLogs');
+});
 
-Route::controller(RequestController::class)
-    /
-    ->group(function () {
-        //get all requests
-        Route::get('/requests', 'getAllRequests');
-        //creating a new request
-        Route::post('/create-request', 'createRequest');
-        Route::get('/request/{id}', 'getSelectedRequest');
-        //grant request
-        Route::post('/grant-request/{id}', 'grantRequest');
-        //decline request
-        Route::post('/decline-request/{id}', 'declineRequest');
-    });
+//Request apis ==================================================================
+Route::controller(RequestController::class)->group(function () {
+    //get all requests
+    Route::get('/requests', 'getAllRequests');
+    //creating a new request
+    Route::post('/create-request', 'createRequest');
+    Route::get('/request/{id}', 'getSelectedRequest');
+    //grant request
+    Route::post('/grant-request/{id}', 'grantRequest');
+    //decline request
+    Route::post('/decline-request/{id}', 'declineRequest');
+});
